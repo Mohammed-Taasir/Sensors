30f44074ac75e46a8f99abd9a3b5155ae11cfb11
Taasir <taasir22029@iiitd.ac.in>
2023-03-06 23:40:41 +0530

    app which detects light magnitude 1.1

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView textView;
-    private TextView textView2;
     private SensorManager sensorManager;
-    private List<Sensor> sensors;
+    private float changedValue;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,26 +27,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text);
-        textView2 = findViewById(R.id.text2);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        printSensors();
-        specificSensor();
     }
 
-    private void specificSensor() {
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null){
-            textView2.setText("theres a heartbeat sensor here !");
-        }else{
-            textView2.setText("theres no heartbeat sensor here !");
-        }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        changedValue = sensorEvent.values[0];
+        textView.setText(String.valueOf(changedValue));
     }
 
-    @SuppressLint("SetTextI18n")
-    private void printSensors() {
-        for(Sensor s : sensors){
-            textView.setText(textView.getText()+"\n"+s.getName());
-        }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
